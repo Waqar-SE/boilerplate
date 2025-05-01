ff09b45cff870fc8ded4bc10681a9f95a0274220
Waqar Ali <waqar_se@outlook.com>
2025-05-01 19:11:53 +0500

    Added Rename Script

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 #
 
+[![Django](https://img.shields.io/badge/Django-5.1.6-brightgreen?logo=django)](https://www.djangoproject.com/)
+[![React](https://img.shields.io/badge/React-18.0.0-blue?logo=react)](https://reactjs.org/)
 [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit)](https://github.com/pre-commit/pre-commit)
+[![Vite](https://img.shields.io/badge/Vite-6.1.0-yellow)](https://vitejs.dev/)
+[![TanStack Router](https://img.shields.io/badge/TanStack_Router-1.109.2-black)](https://tanstack.com/router)
+
+
+
+
+
+## A Boilerplate to kit start Django and React Application development
+
+#### Tech Stack 
+
+- Django
+- React
+
+#### Persistence Layer
+
+Setting the value `DATABASE_URL` correctly in `.env` file will enable the relevant database with the correct database engine installed.
+
+
+#### Package Manager
+- PDM for Python
+- NPM for React
+
+
+### Notable Frontend Packages
+- Tan Stack router
+- React Hook Form
+- Vite
+
+## Setup Instructions
+Fork the repository, clone the forked repo into your system. 
+The very first thing you would want to do is to rename the Django project to name of your liking for which there is a [rename](./scripts/rename.sh) script.
+
+`./scripts/rename.sh <new_name> <old_name>`
+ 
+Old name is optional, in case you want to rename it again.
+
 
--- scripts/rename.sh
+++ scripts/rename.sh
@@ -2,20 +2,42 @@
 
 # Check if the user provided a new string as an argument
 if [ -z "$1" ]; then
-  echo "Please provide a replacement string."
+  echo -e "\033[031;1mPlease provide a replacement string.\033[0m"
+  exit 1
+elif [ -d "./src/$1" ]; then
+  echo -e "\033[031;1mDirectory '$1' already exists.\nplease select another name or remove existing directory if empty\033[0m"
   exit 1
 fi
 
+OLD_NAME="${2:-boilerplate}"
+echo -e "\033[032m\nRenaming '$OLD_NAME' to '$1'......\n\033[0m"
+
 # Define the list of files you want to process
-FILES=("manage.py" "file2.txt" "file3.txt")  # Modify this list with your actual file names
+FILES=("manage.py" "src/$OLD_NAME/settings.py" "src/$OLD_NAME/asgi.py" "src/$OLD_NAME/wsgi.py" "package.json" "pyproject.toml")  # Modify this list with your actual file names
+RENAME_DIR=false
 
 # Loop through each file and replace 'boilerplate' with the new string
+
+
 for FILE in "${FILES[@]}"; do
   if [ -f "$FILE" ]; then
+    if [ "$(grep -i -o "$OLD_NAME" "$FILE" | wc -l)" -gt 0 ]; then
+      RENAME_DIR=true
     # Use sed to replace 'boilerplate' with the given argument
-    sed -i '' "s/boilerplate/$1/g" manage.py
-    echo "Replaced 'boilerplate' with '$1' in $FILE."
+      sed -i '' "s/$OLD_NAME/$1/Ig" $FILE
+      echo "Replaced "$OLD_NAME" with '$1' in $FILE."
+    else
+      echo "No match found in '$FILE'"
+    fi
   else
     echo "File $FILE not found."
   fi
 done
+
+if [ "$RENAME_DIR" = true ]; then
+  echo 'Renaming Project directory'
+  cd "src"
+  mv "$OLD_NAME" "$1"
+fi
+
+echo -e "\033[032m\nRename complete\033[0m"
